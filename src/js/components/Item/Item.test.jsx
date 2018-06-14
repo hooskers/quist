1ddaadba19119f7b34e629a4b20d40bd54f08c6f
Wade Johnson <wadej93@gmail.com>
2018-06-14 04:05:12 +0000

    Add another test and jest-dom matchers

--- src/js/components/Item/Item.test.jsx
+++ src/js/components/Item/Item.test.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { render, Simulate } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
 import Item from '../Item';
+
+test('Item displays title', () => {
+  const { container } = render(
+    <Item id="test_id" title="Test item" checked={false} onDelete={() => {}} />,
+  );
+
+  expect(container.querySelector('.item > span')).toHaveTextContent(
+    'Test item',
+  );
+});
 
 test('clicking delete button calls passed function', () => {
   const onDeleteDummy = jest.fn();
@@ -19,3 +31,5 @@ test('clicking delete button calls passed function', () => {
   expect(onDeleteDummy).toHaveBeenCalledTimes(1);
   expect(onDeleteDummy).toHaveBeenCalledWith('test_id');
 });
+
+test.skip('clicking checkbox calls passed function', () => {});
