620ce2e4931b22e006daeb19518eb52706608cdf
Wade Johnson <wadejohnson@wades-mbp-2.lan>
2018-07-23 21:51:45 -0700

    Add some basic styles to Item component

--- src/js/components/Item/index.jsx
+++ src/js/components/Item/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
 
 const Item = ({ id, title, checked, onCheck, onDelete }) => {
   return (
-    <div className="item">
-      <span>{title}</span>
+    <div css={itemStyles(checked)} className="item">
+      <span className="item-title">{title}</span>
       <input
         type="checkbox"
+        className="item-checkbox"
         title={checked ? 'Uncheck item' : 'Check off item'}
         defaultChecked={checked}
         onChange={e => {
@@ -14,7 +16,11 @@ const Item = ({ id, title, checked, onCheck, onDelete }) => {
           onCheck(id);
         }}
       />
-      <button aria-label="close" onClick={() => onDelete(id)}>
+      <button
+        className="item-delete"
+        aria-label="close"
+        onClick={() => onDelete(id)}
+      >
         Delete
       </button>
     </div>
@@ -28,5 +34,25 @@ Item.propTypes = {
   onCheck: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
 };
+
+const itemStyles = checked => css`
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  padding-top: 5px;
+  padding-bottom: 5px;
+
+  .item-title {
+    margin-left: 7px;
+    text-decoration: ${checked ? 'line-through' : 'unset'};
+  }
+
+  .item-delete {
+    float: right;
+  }
+
+  input[type='checkbox'] {
+    float: left;
+  }
+`;
 
 export default Item;
