237c32fecc82e35204b3a4fc6cd6ead81cc8face
Wade Johnson <wadej93@gmail.com>
2018-07-26 22:15:50 -0700

    Make notifications clickable

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,16 @@
 /* eslint-disable */
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
 
 const token =
-  "flXB0ExPV2Q:APA91bGhHe6s5boxOHw4TNJ2EoXumneQaSkD43MnIeNj0y_0aNrW7gzzSzoQWHAKjK-zazz8M-i8MHskzJk-wqsYJSSXCJsv2E5qWa2p0ymjofViR52pou4c0a8K9ev2XqA0ytWPsij_";
+  'flXB0ExPV2Q:APA91bGhHe6s5boxOHw4TNJ2EoXumneQaSkD43MnIeNj0y_0aNrW7gzzSzoQWHAKjK-zazz8M-i8MHskzJk-wqsYJSSXCJsv2E5qWa2p0ymjofViR52pou4c0a8K9ev2XqA0ytWPsij_';
 
 exports.notifyNewSharedList = functions.firestore
-  .document("lists/{listId}") // Can I use `listId` in the function?
-  .onUpdate(change => {
+  .document('lists/{listId}') // Can I use `listId` in the function? You can! `context.params.listId`!
+  .onUpdate((change, context) => {
     const oldData = change.before.data();
     const newData = change.after.data();
 
@@ -40,23 +40,25 @@ exports.notifyNewSharedList = functions.firestore
       .then(snapshots => {
         return snapshots.forEach(snapshot => {
           const userData = snapshot.data();
+          const link = 'localhost:8080' + '/list/' + context.params.listId;
           const payload = {
             notification: {
-              body: "Click to view list",
-              title: "A list has been shared with you",
-              click_action: "localhost:8080"
+              title: 'A list has been shared with you',
+              body: 'Click to view list',
+              click_action: link,
             },
+            // What is the `data` object supposed to do?
             data: {
-              custom_key_1: "Data for key one",
-              custom_key_2: "Helloooo"
-            }
+              custom_key_1: 'Data for key one',
+              custom_key_2: 'Helloooo',
+            },
           };
 
           Object.keys(userData.fcm_tokens).forEach(token => {
             admin
               .messaging()
               .sendToDevice(token, payload)
-              .then(() => console.log("Message successfully sent."))
+              .then(() => console.log('Message successfully sent.'))
               .catch(response =>
                 console.error(`Error sending message to ${token}`)
               );
