26c9e04313951ff6bdf57b143ed8a436f42c939e
Wade Johnson <wadej93@gmail.com>
2018-07-29 22:36:09 -0700

    Send email when new list is shared!

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,35 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
 
 admin.initializeApp();
 
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  }
+});
+
 const token =
   'flXB0ExPV2Q:APA91bGhHe6s5boxOHw4TNJ2EoXumneQaSkD43MnIeNj0y_0aNrW7gzzSzoQWHAKjK-zazz8M-i8MHskzJk-wqsYJSSXCJsv2E5qWa2p0ymjofViR52pou4c0a8K9ev2XqA0ytWPsij_';
+
+const sendNewSharedListEmail = (email, name, link) => {
+  const mailOptions = {
+    from: 'Bowerlist <noreply@bowerlist.app>',
+    to: email,
+    subject: 'A new list was shared with you!',
+    text: `Hey ${name}! A new list was just shared with you: ${link}`,
+  };
+
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log(`New welcome email sent to: ${email}`);
+  }).catch(e => console.error(e));
+};
 
 exports.notifyNewSharedList = functions.firestore
   .document('lists/{listId}') // Can I use `listId` in the function? You can! `context.params.listId`!
@@ -14,6 +38,7 @@ exports.notifyNewSharedList = functions.firestore
     const oldData = change.before.data();
     const newData = change.after.data();
 
+    // Get only the newly added users
     let newUserIds;
     newUserIds = Object.keys(newData.sharedUsers).filter(newId => {
       return !oldData.sharedUsers.hasOwnProperty(newId);
@@ -53,6 +78,9 @@ exports.notifyNewSharedList = functions.firestore
               custom_key_2: 'Helloooo',
             },
           };
+
+          // Send the email
+          sendNewSharedListEmail(userData.email, userData.name, link);
 
           Object.keys(userData.fcm_tokens).forEach(token => {
             admin
